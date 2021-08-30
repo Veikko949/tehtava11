b4e115480ce9f02a78a787baa4f153cde1010063
Veikko Pihlajamaa <veikko.pihlajamaa@edu.keuda.fi>
2021-08-30 12:49:36 +0300

    Add project files.

---
+++ tehtava11/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace tehtava11
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.heita_button = new System.Windows.Forms.Button();
+            this.noppa1_pictureBox = new System.Windows.Forms.PictureBox();
+            this.noppa2_pictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.noppa3_pictureBox = new System.Windows.Forms.PictureBox();
+            this.noppa4_pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.noppa1_pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.noppa2_pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.noppa3_pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.noppa4_pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // heita_button
+            // 
+            this.heita_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.heita_button.Location = new System.Drawing.Point(59, 278);
+            this.heita_button.Name = "heita_button";
+            this.heita_button.Size = new System.Drawing.Size(169, 51);
+            this.heita_button.TabIndex = 0;
+            this.heita_button.Text = "Heitä";
+            this.heita_button.UseVisualStyleBackColor = true;
+            this.heita_button.Click += new System.EventHandler(this.heita_button_Click);
+            // 
+            // noppa1_pictureBox
+            // 
+            this.noppa1_pictureBox.Image = global::tehtava11.Properties.Resources._1;
+            this.noppa1_pictureBox.Location = new System.Drawing.Point(34, 65);
+            this.noppa1_pictureBox.Name = "noppa1_pictureBox";
+            this.noppa1_pictureBox.Size = new System.Drawing.Size(104, 96);
+            this.noppa1_pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.noppa1_pictureBox.TabIndex = 1;
+            this.noppa1_pictureBox.TabStop = false;
+            // 
+            // noppa2_pictureBox
+            // 
+            this.noppa2_pictureBox.Image = global::tehtava11.Properties.Resources._1;
+            this.noppa2_pictureBox.Location = new System.Drawing.Point(144, 65);
+            this.noppa2_pictureBox.Name = "noppa2_pictureBox";
+            this.noppa2_pictureBox.Size = new System.Drawing.Size(104, 96);
+            this.noppa2_pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.noppa2_pictureBox.TabIndex = 2;
+            this.noppa2_pictureBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 37);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nopan Heitto";
+            // 
+            // noppa3_pictureBox
+            // 
+            this.noppa3_pictureBox.Image = global::tehtava11.Properties.Resources._6;
+            this.noppa3_pictureBox.Location = new System.Drawing.Point(34, 167);
+            this.noppa3_pictureBox.Name = "noppa3_pictureBox";
+            this.noppa3_pictureBox.Size = new System.Drawing.Size(104, 96);
+            this.noppa3_pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.noppa3_pictureBox.TabIndex = 4;
+            this.noppa3_pictureBox.TabStop = false;
+            // 
+            // noppa4_pictureBox
+            // 
+            this.noppa4_pictureBox.Image = global::tehtava11.Properties.Resources._6;
+            this.noppa4_pictureBox.Location = new System.Drawing.Point(144, 167);
+            this.noppa4_pictureBox.Name = "noppa4_pictureBox";
+            this.noppa4_pictureBox.Size = new System.Drawing.Size(104, 96);
+            this.noppa4_pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.noppa4_pictureBox.TabIndex = 5;
+            this.noppa4_pictureBox.TabStop = false;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 352);
+            this.Controls.Add(this.noppa4_pictureBox);
+            this.Controls.Add(this.noppa3_pictureBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.noppa2_pictureBox);
+            this.Controls.Add(this.noppa1_pictureBox);
+            this.Controls.Add(this.heita_button);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.noppa1_pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.noppa2_pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.noppa3_pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.noppa4_pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button heita_button;
+        private System.Windows.Forms.PictureBox noppa1_pictureBox;
+        private System.Windows.Forms.PictureBox noppa2_pictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox noppa3_pictureBox;
+        private System.Windows.Forms.PictureBox noppa4_pictureBox;
+    }
+}
+
---
+++ tehtava11/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tehtava11
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void heita_button_Click(object sender, EventArgs e)
+        {
+            piiraNoppa(noppa1_pictureBox);
+            piiraNoppa(noppa2_pictureBox);
+            piiraNoppa(noppa3_pictureBox);
+            piiraNoppa(noppa4_pictureBox);
+        }
+        private void piiraNoppa(PictureBox Noppat)
+        {
+            Random heita = new Random();
+            int noppa = heita.Next(1, 7);
+            switch (noppa)
+            {
+                case 1:
+                    Noppat.Image = Properties.Resources._1;
+                    break;
+                case 2:
+                    Noppat.Image = Properties.Resources._2;
+                    break;
+                case 3:
+                    Noppat.Image = Properties.Resources._3;
+                    break;
+                case 4:
+                    Noppat.Image = Properties.Resources._4;
+                    break;
+                case 5:
+                    Noppat.Image = Properties.Resources._5;
+                    break;
+                case 6:
+                    Noppat.Image = Properties.Resources._6;
+                    break;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
